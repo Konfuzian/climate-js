1cebb3e92c6973217fa43367d91b246cb811f405
Konfuzian <sebastian.hofer@gmx.net>
2022-01-08 21:15:35 +0100

    Add TODO.

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express()
 app.get('/', (req, res) => (res.json('Welcome')))
 
 app.get('/news', (req, res) => {
-
+    // TODO: fetch from https://www.theguardian.com/environment/climate-crisis
+    // $$('.js-headline-text').map((x) => x.text).filter((x) => x != undefined)
 })
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
